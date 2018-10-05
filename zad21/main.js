2bdafbc916f8bd48143776371c9d5b125c78739d
Aleksandra Borczak <ola.borczak@gmail.com>
2018-10-05 22:09:10 +0200

    zadania-js

---
+++ zad21/main.js
@@ -0,0 +1,6 @@
+var zmienna12 = 12;
+if (zmienna12 > 10){
+    console.log("Podana liczba jest większa od 10.");
+}else{
+    var napis = "Podana liczba nie ejst większa od 10.";
+}
---
+++ zad22/js/main.js
@@ -0,0 +1,6 @@
+var liczbaPierwsza = 3;
+var liczbaDruga = 5;
+function multiply(liczbaPierwsza, liczbaDruga) {
+var suma = liczbaPierwsza * liczbaDruga;
+return suma;
+}
