d714aa5a7d1a894207f16ccacf3cc1f6dbb85f1b
Aleksandra Borczak <ola.borczak@gmail.com>
2018-10-09 23:48:06 +0200

    zadanie28

---
+++ zad28/main.js
@@ -0,0 +1,8 @@
+function funkcja1(n){
+    if(n >= 100 && n <= 200){
+        console.log( "Liczba znajduje się w przedziale");
+    } else {
+        console.log ( "Liczba nie znajduje się w przedziale");
+    }
+}
+funkcja1(7);
