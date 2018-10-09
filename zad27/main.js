ad5fee78c2dbb0ba1bfb67a67cd3e33beb62bad3
Aleksandra Borczak <ola.borczak@gmail.com>
2018-10-09 23:24:09 +0200

    zadanie27

---
+++ zad27/main.js
@@ -0,0 +1,11 @@
+let zmienna1 = "Uwielbiam JavaScript";
+let zmienna2 = "Jestem świetnym programistą";
+
+function lengthOfStrings(zmienna1, zmienna2){
+    if(zmienna1.length>zmienna2.length){
+        console.log(zmienna1);
+    }else{
+        console.log(zmienna2);
+    }
+}
+lengthOfStrings(zmienna1, zmienna2)
