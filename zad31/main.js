40a28c80584cae3a7faa23cd3a5861d6c748bcb7
Aleksandra Borczak <ola.borczak@gmail.com>
2018-10-10 13:31:36 +0200

    zadanie31

---
+++ zad31/main.js
@@ -0,0 +1,17 @@
+function mathOperations(a, b){
+    if((a + b )> 0){
+        console.log('Wynik dodawania wynosi '+ (a + b));
+    } else{
+        console.log('Wynik jest ujemny.');
+    }
+      if((a - b )> 0){
+        console.log('Wynik odejmowania wynosi '+ (a - b));
+    } else{
+        console.log('Wynik jest ujemny.');
+    }  if((a * b )> 0){
+        console.log('Wynik mnożenia wynosi '+ (a * b));
+    } else{
+        console.log('Wynik jest ujemny.');
+    }
+}
+mathOperations(71, 8);
