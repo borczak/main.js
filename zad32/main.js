21774802c24d7974121e05c69be5930cadfea11a
Aleksandra Borczak <ola.borczak@gmail.com>
2018-10-10 13:57:52 +0200

    zad32

---
+++ zad32/main.js
@@ -0,0 +1,9 @@
+let tablica1 = [9, 5, 10, 6, 13];
+
+function elementOfTablica1(tablica1) {
+    let result = tablica1[0];
+    return result;
+}
+
+elementOfTablica1(tablica1);
+console.log(elementOfTablica1(tablica1));
