7d636cb71c5a505b3969302bca387667477f58a3
Aleksandra Borczak <ola.borczak@gmail.com>
2018-10-10 18:58:50 +0200

    zadanie30

--- zad30/js/main.js
+++ zad30/js/main.js
@@ -2,9 +2,9 @@ let button1 = document.getElementById("dodaj");
 let button2 = document.getElementById("usun");
 let tekst1 = document.getElementById("tekst");
 
-button1.onclick = function() {
+button1.onclick = function () {
     tekst1.textContent = "Akademia108";
 }
-button2.onclick = function() {
+button2.onclick = function () {
     tekst1.textContent = "";
 }
